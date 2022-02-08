0a989c4581a294e98845753925983378d855aa00
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-07 23:15:04 -0500

    [cauthflow_tests] Init (to replicate vcpkg install bug on windows)

---
+++ cauthflow_tests/test_cauthflow.h
@@ -0,0 +1,27 @@
+#include <greatest.h>
+#include <stdbool.h>
+#include <cauthflow_configure.h>
+
+#include <google_auth.h>
+
+#ifdef _MSC_VER
+#define NUM_FORMAT "%zu"
+typedef size_t num_type;
+#elif defined(__linux__) || defined(linux) || defined(__linux)
+#define NUM_FORMAT "%d"
+typedef int num_type;
+#else
+#define NUM_FORMAT "%lu"
+typedef unsigned long num_type;
+#endif /* _MSC_VER */
+
+TEST x_is_directory_should_be_true(void) {
+    get_google_auth(CLIENT_ID, CLIENT_SECRET,  /*refresh_token*/ NULL);
+    ASSERT_EQ(0, 0);
+    PASS();
+}
+
+/* Suites can group multiple tests with common setup. */
+SUITE (cauthflow_suite) {
+    RUN_TEST(x_is_directory_should_be_true);
+}
